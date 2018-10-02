0d1fb7bb3913c66c321287fe7c3c14f322d62800
Administrator <Administrator@192.168.145.131>
2018-10-02 18:10:25 +0200

    empezamos a conectar la BBDD para el login

--- login.php
+++ login.php
@@ -2,20 +2,26 @@
 include ('misFunciones.php');
 
 $mysqli = conectaBBDD();
-
- $cajaNombre = $_POST['cajaNombre'];
- 
- $cajaPassword = $_POST['cajaPassword'];
-
-
+$cajaNombre = $_POST['cajaNombre'];
+$cajaPassword = $_POST['cajaPassword'];
 
 //echo 'Has escrito el usuario: '.$cajaNombre.' y la contraseña: '.$cajaPassword; 
  
- $resultadoQuery = $mysqli -> query("SELECT * FROM preguntas");
+$resultadoQuery = $mysqli -> query("SELECT * FROM usuarios WHERE nombreUsuario='$cajaNombre' AND userPass='$cajaPassword'");
  
- $numPreguntas = $resultadoQuery -> num_rows;
+$numUsuarios = $resultadoQuery -> num_rows;
  
-for ( $i = 0; $i < $numPreguntas; $i++){
+for ( $i = 0; $i < $numPreguntas; $i++)
+{
     $r = $resultadoQuery -> fetch_array();
-    echo $r['enunciado'] .'<br/>';
+    echo $r['nombreUsuario'] .'<br/>';
 }
+
+//if($numUsuarios > 0)
+//{
+//    //muestro la pantalla de la aplicación
+//}
+//else 
+//{
+//     
+//}
--- misFunciones.php
+++ misFunciones.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectaBBDD(){
-    require('configuracion.php');
+    require('./configuracion.php');
     $mysqli = new mysqli($servidor, $usuario_mysql,  $clave_mysql, $bbdd);
     $mysqli -> query("SET NAMES utf8");
     return $mysqli;
